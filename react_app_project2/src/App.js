d37974e289a8ef9bc6c6f31146c12da4c73d83ca
NorahAyad <74140378+NorahAyad@users.noreply.github.com>
2021-11-07 18:51:07 +0300

    frist edit

--- react_app_project2/src/App.js
+++ react_app_project2/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Routes,Route,} from "react-router-dom";
 
+import Navr from './component/Navr';
+import Home from "./component/Home";
+
+
+ import Services from './component/Services'
+
+
+ import Occasions from './component/Occasions'
+import About from './component/About'
+import OurGoals from './component/OurGoals'
+import Favorite from "./component/Favorite";
+import Login from "./component/Login";
+import ShoppingCart from './component/shoppingCart'
+import Connect from "./component/Connect";
+import Footer from "./component/Footer";
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navr/>
+     <Routes>
+          <Route  path="/" element={<Home />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Occasions" element={<Occasions />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/OurGoals" element={<OurGoals />} />
+          <Route path="/Connect" element={<Connect />} />
+          <Route path="/Favorite" element={<Favorite/>} />
+          <Route path="/shoppingCart" element={<ShoppingCart/>} />
+          <Route path="/Login" element={<Login/>} />
+        </Routes>
+       
+        <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
