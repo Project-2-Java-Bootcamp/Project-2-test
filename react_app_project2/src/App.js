7934eae272a0d948413d67356bbc858464554bac
nahidfawaz123 <n.alshouf@gmail.com>
2021-11-07 15:15:21 +0300

    v01

--- react_app_project2/src/App.js
+++ react_app_project2/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './styles.css'
+import React from 'react';
+import Slider from '../src/home/Slider';
+import Service from '../src/home/Service';
+import Sections from '../src/home/Sections'
+import Masks from '../src/home/Masks'
+import Dance from '../src/home/Dance'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <>
+  <Sections/>
+  <Masks />
+  <Dance />
+  <Slider />
+  <br/><Service />
+  <br/>
+  </>
   );
 }
 
